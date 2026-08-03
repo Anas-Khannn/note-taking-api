2523fbc8d190137efad271d82b6c6b3abd462236
Anas Khan <anaskhanislamian728@gmail.com>
2026-08-03 22:09:31 +0500

    refactor: move backend into dedicated directory

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,11 @@ const errorHandler = require(
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use("/api", router);
